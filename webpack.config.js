9e0edfcb36cd077203aeb2e832fa0934f383087b
Em3k <em3k@protonmail.com>
2017-06-19 19:14:01 +0100

    Added html-webpack-plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,17 @@
+const webpack = require('webpack');
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
 module.exports = {
     entry: './src/assets/js/app.js',
     output: {
-        filename:   './dist/assets/js/app.bundle.js'
-    }
+        path: path.resolve(__dirname, 'dist'),
+        //path: './dist',
+        filename: 'app.bundle.js'
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: 'New Webpack 2 Web Project',
+            template: './src/assets/index.ejs',
+        })]
 }
