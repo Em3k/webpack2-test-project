df97ba567bd28ac5c8fd457bdff062a7529be84b
Em3k <em3k@protonmail.com>
2017-06-20 15:36:25 +0100

    Added babel, dev-server, file-loader

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,14 @@
 const webpack = require('webpack');
 const path = require('path');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+const extractPlugin = new ExtractTextPlugin({
+    filename: 'app.css'
+});
+
+
 
 module.exports = {
     entry: './src/assets/js/app.js',
@@ -12,18 +20,55 @@ module.exports = {
     module: {
         rules: [
             {
-                test: /\.css$/,
-                use: ['style-loader', 'css-loader']
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: 'babel-loader'
+            },
+            {
+                test: /\.scss$/,
+                use: extractPlugin.extract({
+                    use: ['css-loader', 'sass-loader']
+                })
+            },
+            {
+                test: /\.html$/,
+                use: ['html-loader']
+            },
+            {
+                test: /\.(jpg|png)$/,
+                use: [
+                    {
+                        loader: 'file-loader',
+                        options: {
+                            name: '[name].[ext]',
+                            outputPath: 'assets/img',
+                            publicPath: 'assets/img'
+                        }
+                    }
+                ]
             }
         ]
     },
+    devServer: {
+        contentBase: path.join(__dirname, "dist"),
+        compress: true,
+        //port: 9000,
+        open: true
+    },
     plugins: [
+        extractPlugin,
         new HtmlWebpackPlugin({
             title: 'New Webpack 2 Web Project',
-            minify: {
-                collapseWhitespace: false
-            },
-            hash: false,
-            template: './src/assets/index.ejs',
-        })]
+            template: './src/index.ejs',
+            filename: './index.html',
+            hash: false
+            // minify: {
+            //     collapseWhitespace: false
+            // },
+        }),
+        new webpack.optimize.UglifyJsPlugin({
+            // ...
+        }),
+        new CleanWebpackPlugin(['dist'])
+    ]
 }
